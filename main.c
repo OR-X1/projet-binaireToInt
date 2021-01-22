4488b1570cfc82386f8cf732d50337c0cd30d79f
Othmane_Rhazlani <oghazlani0pgmail.com>
2021-01-22 10:34:52 +0100

    function bin to int

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int max(int binaire, int nombre) {
+
+   int result;
+ 
+   //////// binaire to int
+ 
+   return result; 
+}
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	return 0;
+}
